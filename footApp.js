2ec11f7fa22f032729efebf49ddda42db096b501
Nastya <artem.matviienko23@gmail.com>
2022-08-01 13:14:34 +0300

    Update html file and js file

--- footApp.js
+++ footApp.js
@@ -1,1 +1,15 @@
+function GetChoice() {
+    let choice = document.getElementById('season').value;
+    if (choice == 1) {
+        // console.log('Ничего не выбрано');
+    } else if (choice == 2) {
+       document.getElementById('season2').display ="flex";
+        // console.log('Сезон 2010/2011');
+    } else {
+        document.getElementById('season3').display ="flex";
+    }
+}
 
+// function GetSeason() {
+//
+// }
