5799638c4d37e062cf8d90f37899661fb08b251c
Анастасия Гриценко <gricenkoanastasia05@gmail.com>
2022-08-02 18:51:35 +0300

    Changing the Football Season Selection Option

--- footApp.js
+++ footApp.js
@@ -1,15 +1,17 @@
 function GetChoice() {
     let choice = document.getElementById('season').value;
     if (choice == 1) {
+        document.getElementsByClassName('season2')[0].style.display ="none";
+        document.getElementsByClassName('season3')[0].style.display ="none";
         // console.log('Ничего не выбрано');
     } else if (choice == 2) {
-       document.getElementById('season2').display ="flex";
+        document.getElementsByClassName('season2')[0].style.display ="flex";
+        document.getElementsByClassName('season3')[0].style.display ="none";
+        // document.getElementById('season2').display ="flex";
         // console.log('Сезон 2010/2011');
     } else {
-        document.getElementById('season3').display ="flex";
+        document.getElementsByClassName('season3')[0].style.display ="flex";
+        document.getElementsByClassName('season2')[0].style.display ="none";
+
     }
 }
-
-// function GetSeason() {
-//
-// }
